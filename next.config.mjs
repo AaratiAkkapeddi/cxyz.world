e821924f1d143b4add3beb61a3c5c032e61d7b52
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-10-03 13:22:04 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const config = {
   images: { remotePatterns: [{ hostname: 'cdn.sanity.io' }] },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default config
