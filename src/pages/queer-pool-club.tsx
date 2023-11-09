64c3d49f2f2940bc685551a97edd2c15cfa5a15e
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-11-08 20:24:29 -0500

    qpc changes

--- src/pages/queer-pool-club.tsx
+++ src/pages/queer-pool-club.tsx
@@ -1,11 +1,14 @@
 import { PortableText } from '@portabletext/react'
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import { useLiveQuery } from 'next-sanity/preview'
 import QpcContainer from '~/components/qpcContainer'
 import { readToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
 import { urlForImage } from '~/lib/sanity.image'
+import gif from '~/components/qpc/qpc.gif'
+
 import {
   getQpc,
   type Qpc,
@@ -52,8 +55,30 @@ export default function qpcPage(
     slug: "props.qpc.slug.current",
   })
 
+  useEffect(() => {
 
+    // if(typeof window != `undefined`){
+    //   setInterval(function(){
+    //     let imgs = document.querySelectorAll(".qpc-item");
+    //     let onIndex = 0;
+    //     imgs.forEach((img,i) => {
+    //       if(img.classList.contains("on")){
+    //         onIndex = i;
+    //         img.classList.remove("on");
+    //       }
+    //     })
+    //     if(onIndex == imgs.length -1){
+    //       imgs[imgs.length - 1 ].classList.add("on")
+    //     }else{
+    //       imgs[onIndex + 1 ].classList.add("on")
+    //     }
+    //   },1200)
 
+      
+    // }
+    
+    
+  }, [])
 
 
   return (
@@ -63,6 +88,12 @@ export default function qpcPage(
         <div className="page__container">
           
           <div className="page__content">
+          <div className="qpc-carousel">
+            <div  className="qpc-item on">
+              <Image src={gif} alt="Queer Pool Club Posters" />
+            </div>
+         
+        </div>
             <div className="subnav">
               <div>
                 <PortableText value={qpc.upcomingInfo} />
@@ -72,9 +103,9 @@ export default function qpcPage(
             
           </div>
         </div>
-        <ul>
+        
 
-        </ul>
+        
         
       </section>
     </QpcContainer>
